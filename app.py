70394b41f1ba36966f413221948e49f030d7a2cb
Jesse Ma <151305994+Festivius@users.noreply.github.com>
2025-01-12 08:45:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def run_python():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port,debug=True, use_reloader=False)
